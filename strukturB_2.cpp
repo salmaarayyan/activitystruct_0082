f10e1e938a70d35c3c279dfb0eefb9ce32b5c56b
salmaarayyan <salmaarayyan@gmail.com>
2024-04-01 14:24:28 +0700

    menampilkan kalimat nama desa

--- strukturB_2.cpp
+++ strukturB_2.cpp
@@ -13,3 +13,25 @@ struct Mahasiswa {
     DetailAlamat alamat;
     int umur;
 };
+
+int main () {
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    
+    cout << "Alamat Mahasiswa : ";
+    cout << "\t Nama Desa : ";
+    
+
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n Nim : " << mhs.nim;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n Alamat : " << mhs.alamat;
+    cout << "\n Umur : " << mhs.umur;
+
+}
