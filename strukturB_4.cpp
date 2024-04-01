3fa23332b48984ef1f4794594f1ea8ce296aa7a3
salmaarayyan <salmaarayyan@gmail.com>
2024-04-01 15:21:16 +0700

    menampilkan kalimat data mahasiswa

--- strukturB_4.cpp
+++ strukturB_4.cpp
@@ -35,15 +35,18 @@ int main()
         cout << "Umur Mahasiswa : ";
         cin >> mhs[i].umur;
         cin.ignore(1, '\n');
+        cout << endl;
     }
 
     for (int i = 0; i < 3; i++){
     cout << endl;
+    cout << "Data Mahasiswa ke - " << (i+1) << " : " << endl;
     cout << "\n Nim : " << mhs[i].nim;
     cout << "\n Nama : " << mhs[i].nama;
     cout << "\n Alamat : ";
     cout << "\n \t Desa : " << mhs[i].alamat.desa;
     cout << "\n \t Kota : " << mhs[i].alamat.kota;
     cout << "\n Umur : " << mhs[i].umur;
+    cout << endl;
     }
 }
