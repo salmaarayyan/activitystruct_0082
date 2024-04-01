2fb028b3b73d88248b66799d563d685cd27632ba
salmaarayyan <salmaarayyan@gmail.com>
2024-04-01 14:17:29 +0700

    membuat struktur detail alamat

---
+++ strukturC_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat
+{
+    string desa;
+    
+};
+
+struct Mahasiswa {
+    string nim;
+    string nama;
+    string alamat;
+    int umur;
+};
