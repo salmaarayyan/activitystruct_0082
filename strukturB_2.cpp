84e7b7b695ac16f0d947d36fcd6f891d450b3f14
salmaarayyan <salmaarayyan@gmail.com>
2024-04-01 14:32:25 +0700

    menambahkan endl di alamat mahasiswa

--- strukturB_2.cpp
+++ strukturB_2.cpp
@@ -21,7 +21,7 @@ int main () {
     cout << "Nama Mahasiswa : ";
     cin >> mhs.nama;
     
-    cout << "Alamat Mahasiswa : ";
+    cout << "Alamat Mahasiswa : " << endl;
     cout << "\t Nama Desa : ";
     cin >> mhs.alamat.desa;
     cout << "\t Nama Kota : ";
